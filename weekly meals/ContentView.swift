75b03b67240f4fadacdf64f85b0f034dc94fc285
Rafał Piechowicz <rpiechowicz@icloud.com>
2026-02-03 14:55:43 +0100

    Initial Commit

---
+++ weekly meals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weekly meals
+//
+//  Created by Rafi on 03/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weekly meals/weekly_mealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  weekly_mealsApp.swift
+//  weekly meals
+//
+//  Created by Rafi on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct weekly_mealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
